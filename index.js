6580ed8d23a28184b8493bd830d6b285042d2c80
Temitope Alabi <temitopealabi@Temitopes-MacBook-Pro.local>
2018-04-11 15:17:21 -0700

    First commit

---
+++ index.js
@@ -0,0 +1,136 @@
+const ethUtil = require('ethereumjs-util')
+const ethAbi = require('ethereumjs-abi')
+const mmdecrypt = require("meta-mask-decrypt")
+
+// const encryptedData = { iv: '520f202afe4ab4a506c516b59ff23fcb',
+//   ephemPublicKey: '042e150e1d5eb888fe604d5052a767aea76f43e351dd698a89a96ae3da13501e5ff61b6225ea9af88e13aaccbca0c163785110c265c3d5e8adb0ff0a5ac803db9f',
+//   ciphertext: '541858a38724fe92deb9472b82909d43fb5fcda95db124dc4b298664f3d3878563f7243be561eddd8c029d92da2a8acfad6001c4829f567c6919d1a825813783bc9e4ca2f2d50d606a71abf061a4e34d7b9c9e1dc16daf350d801cd4be2476e42934203cd556b250126f938257c45ade2286bdd3560ca7185469a0630dedac42b78ed1010afc5a34f9e9a9bc53ea8a12e8aa01fd7e618e08fa7ac9afa87ac233381e545eb3138944eb6168b9ebcd6c8949e853278f56f3a4284bf9e94dfd0ba4',
+//   mac: 'e99e453885fe1eaac74d37f56b20da561806ec6bf74a81e66f32efea6f5ad80f' }
+
+// const pivkey = '0x7e5374ec2ef0d91761a6e72fdf8f6ac665519bfdf6da0a2329cf0d804514b816';
+
+module.exports = {
+
+  concatSig: function (v, r, s) {
+    const rSig = ethUtil.fromSigned(r)
+    const sSig = ethUtil.fromSigned(s)
+    const vSig = ethUtil.bufferToInt(v)
+    const rStr = padWithZeroes(ethUtil.toUnsigned(rSig).toString('hex'), 64)
+    const sStr = padWithZeroes(ethUtil.toUnsigned(sSig).toString('hex'), 64)
+    const vStr = ethUtil.stripHexPrefix(ethUtil.intToHex(vSig))
+    return ethUtil.addHexPrefix(rStr.concat(sStr, vStr)).toString('hex')
+  },
+
+  normalize: function (input) {
+    if (!input) return
+
+    if (typeof input === 'number') {
+      const buffer = ethUtil.toBuffer(input)
+      input = ethUtil.bufferToHex(buffer)
+    }
+
+    if (typeof input !== 'string') {
+      var msg = 'eth-sig-util.normalize() requires hex string or integer input.'
+      msg += ' received ' + (typeof input) + ': ' + input
+      throw new Error(msg)
+    }
+
+    return ethUtil.addHexPrefix(input.toLowerCase())
+  },
+
+  personalSign: function (privateKey, msgParams) {
+    var message = ethUtil.toBuffer(msgParams.data)
+    var msgHash = ethUtil.hashPersonalMessage(message)
+    var sig = ethUtil.ecsign(msgHash, privateKey)
+    var serialized = ethUtil.bufferToHex(this.concatSig(sig.v, sig.r, sig.s))
+    return serialized
+  },
+
+  recoverPersonalSignature: function (msgParams) {
+    const publicKey = getPublicKeyFor(msgParams)
+    const sender = ethUtil.publicToAddress(publicKey)
+    const senderHex = ethUtil.bufferToHex(sender)
+    return senderHex
+  },
+
+  extractPublicKey: function (msgParams) {
+    const publicKey = getPublicKeyFor(msgParams)
+    return '0x' + publicKey.toString('hex')
+  },
+
+  typedSignatureHash: function (typedData) {
+    const hashBuffer = typedSignatureHash(typedData)
+    return ethUtil.bufferToHex(hashBuffer)
+  },
+
+  signTypedData: function (privateKey, msgParams) {
+    const msgHash = typedSignatureHash(msgParams.data)
+    const sig = ethUtil.ecsign(msgHash, privateKey)
+    return ethUtil.bufferToHex(this.concatSig(sig.v, sig.r, sig.s))
+  },
+
+  recoverTypedSignature: function (msgParams) {
+    const msgHash = typedSignatureHash(msgParams.data)
+    const publicKey = recoverPublicKey(msgHash, msgParams.sig)
+    const sender = ethUtil.publicToAddress(publicKey)
+    return ethUtil.bufferToHex(sender)
+  }
+
+  decrypt: async function (encryptedMsg, privateKey){
+    const decrypted = await mmdecrypt.decryptWithPrivateKey (privateKey, encryptedMsg);
+    
+    const decryptedPayload = JSON.parse(decrypted)
+    console.log(decryptedPayload.message);
+  }
+
+}
+
+
+
+/**
+ * @param typedData - Array of data along with types, as per EIP712.
+ * @returns Buffer
+ */
+function typedSignatureHash(typedData) {
+  const error = new Error('Expect argument to be non-empty array')
+  if (typeof typedData !== 'object' || !typedData.length) throw error
+
+  const data = typedData.map(function (e) {
+    return e.type === 'bytes' ? ethUtil.toBuffer(e.value) : e.value
+  })
+  const types = typedData.map(function (e) { return e.type })
+  const schema = typedData.map(function (e) {
+    if (!e.name) throw error
+    return e.type + ' ' + e.name
+  })
+
+  return ethAbi.soliditySHA3(
+    ['bytes32', 'bytes32'],
+    [
+      ethAbi.soliditySHA3(new Array(typedData.length).fill('string'), schema),
+      ethAbi.soliditySHA3(types, data)
+    ]
+  )
+}
+
+function recoverPublicKey(hash, sig) {
+  const signature = ethUtil.toBuffer(sig)
+  const sigParams = ethUtil.fromRpcSig(signature)
+  return ethUtil.ecrecover(hash, sigParams.v, sigParams.r, sigParams.s)
+}
+
+function getPublicKeyFor (msgParams) {
+  const message = ethUtil.toBuffer(msgParams.data)
+  const msgHash = ethUtil.hashPersonalMessage(message)
+  return recoverPublicKey(msgHash, msgParams.sig)
+}
+
+
+function padWithZeroes (number, length) {
+  var myString = '' + number
+  while (myString.length < length) {
+    myString = '0' + myString
+  }
+  return myString
+}
+
---
+++ meta-mask-decrypt.js
@@ -0,0 +1,113 @@
+
+//original decrypt function takes encrypted msg, and 
+// Decrypt = async function (Encryptedmsg, privateKey){
+// 	const decrypted = await ethCypto.decryptWithPrivateKey (privateKey, encryptedmsg);
+	
+// 	const decryptedPayload = JSON.parse(decrypted)
+// 	console.log(decryptedPayload.message);
+// }
+var crypto = require("crypto");
+var promise = typeof Promise === "undefined" ?
+              require("es6-promise").Promise :
+              Promise;
+var secp256k1 = require("secp256k1")
+var ecdh = require("./ecdh")
+
+const e = { iv: '520f202afe4ab4a506c516b59ff23fcb',
+  ephemPublicKey: '042e150e1d5eb888fe604d5052a767aea76f43e351dd698a89a96ae3da13501e5ff61b6225ea9af88e13aaccbca0c163785110c265c3d5e8adb0ff0a5ac803db9f',
+  ciphertext: '541858a38724fe92deb9472b82909d43fb5fcda95db124dc4b298664f3d3878563f7243be561eddd8c029d92da2a8acfad6001c4829f567c6919d1a825813783bc9e4ca2f2d50d606a71abf061a4e34d7b9c9e1dc16daf350d801cd4be2476e42934203cd556b250126f938257c45ade2286bdd3560ca7185469a0630dedac42b78ed1010afc5a34f9e9a9bc53ea8a12e8aa01fd7e618e08fa7ac9afa87ac233381e545eb3138944eb6168b9ebcd6c8949e853278f56f3a4284bf9e94dfd0ba4',
+  mac: 'e99e453885fe1eaac74d37f56b20da561806ec6bf74a81e66f32efea6f5ad80f' }
+
+const pk = '0x7e5374ec2ef0d91761a6e72fdf8f6ac665519bfdf6da0a2329cf0d804514b816';
+
+var decrypt = async function (encryptedMsg, privateKey){
+    const decrypted = await decryptWithPrivateKey (privateKey, encryptedMsg);
+    
+    const decryptedPayload = JSON.parse(decrypted)
+    console.log(decryptedPayload.message);
+}
+
+
+
+// decryptWithPrivateKey function
+//import eccrypto 
+var decryptWithPrivateKey = async function(privateKey, encrypted) {
+
+    // remove trailing '0x' from privateKey
+    const twoStripped = privateKey.replace(/^.{2}/g, '');
+
+    const encryptedBuffer = {
+        iv: new Buffer(encrypted.iv, 'hex'),
+        ephemPublicKey: new Buffer(encrypted.ephemPublicKey, 'hex'),
+        ciphertext: new Buffer(encrypted.ciphertext, 'hex'),
+        mac: new Buffer(encrypted.mac, 'hex')
+    };
+
+    const decryptedBuffer = await eccdecrypt(
+        new Buffer(twoStripped, 'hex'),
+        encryptedBuffer
+    );
+    return decryptedBuffer.toString();
+}
+
+//eccrypto.decrypt
+// var promise = require promise;
+
+// secp256k1 = require("secp256k1")
+//ecdh = require("./build/Release/ecdh")
+
+function sha512(msg) {
+  return crypto.createHash("sha512").update(msg).digest();
+}
+
+function hmacSha256(key, msg) {
+  return crypto.createHmac("sha256", key).update(msg).digest();
+}
+
+var derive = function(privateKeyA, publicKeyB) {
+  return new promise(function(resolve) {
+    resolve(ecdh.derive(privateKeyA, publicKeyB));
+  });
+};
+
+function assert(condition, message) {
+  if (!condition) {
+    throw new Error(message || "Assertion failed");
+  }
+}
+
+function aes256CbcDecrypt(iv, key, ciphertext) {
+  var cipher = crypto.createDecipheriv("aes-256-cbc", key, iv);
+  var firstChunk = cipher.update(ciphertext);
+  var secondChunk = cipher.final();
+  return Buffer.concat([firstChunk, secondChunk]);
+}
+
+function equalConstTime(b1, b2) {
+  if (b1.length !== b2.length) {
+    return false;
+  }
+  var res = 0;
+  for (var i = 0; i < b1.length; i++) {
+    res |= b1[i] ^ b2[i];  // jshint ignore:line
+  }
+  return res === 0;
+}
+
+eccdecrypt = function(privateKey, opts) {
+  return derive(privateKey, opts.ephemPublicKey).then(function(Px) {
+    var hash = sha512(Px);
+    var encryptionKey = hash.slice(0, 32);
+    var macKey = hash.slice(32);
+    var dataToMac = Buffer.concat([
+      opts.iv,
+      opts.ephemPublicKey,
+      opts.ciphertext
+    ]);
+    var realMac = hmacSha256(macKey, dataToMac);
+    assert(equalConstTime(opts.mac, realMac), "Bad MAC");
+    return aes256CbcDecrypt(opts.iv, encryptionKey, opts.ciphertext);
+  });
+};
+
+decrypt(e, pk);
